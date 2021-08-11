82a9630f1abb689c1466483c023b05def344db32
Stefan Sjöman <stefan.sjoman@hotmail.com>
2021-08-11 12:00:33 +0200

    installed redux and toolkit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+	const [count, setCount] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+				<h1>TESTA REDUX</h1>
       </header>
+			<div>
+				<div>count: { count }</div>
+				<button onClick={ () => setCount(count + 1) }>increase</button>
+				<button onClick={ () => setCount(count -1) }>decrease</button>
+			</div>
     </div>
   );
 }
